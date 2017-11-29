d45c45221f1f6d8c1738456212c2a5ab45dab7a6
fhergomez <fhergomez37@gmail.com>
2017-11-28 16:52:27 -0800

    add files to repo

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+	render() {
+		return (
+			<h2 className="NotFound">Not Found!!!!</h2>
+		)
+	}
+}
+
+export default NotFound;
